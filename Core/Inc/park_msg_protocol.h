a84ce48c51e403d419ad4373a139bb10cb6d70cf
tamar <tamar@DESKTOP-36Q4TJ1.localdomain>
2026-01-02 13:01:45 -0500

    Add GPS Generator STM32 project

---
+++ Core/Inc/park_msg_protocol.h
@@ -0,0 +1,22 @@
+#ifndef PARK_MESSAGE_H
+#define PARK_MESSAGE_H
+
+#include <stdint.h>    // for fixed-size integer types if needed
+#include <time.h>      // for time_t
+
+typedef enum {
+    PARK_END = 0,
+    PARK_START = 1,
+} Parking;
+
+#pragma pack(1)  // Disable padding
+typedef struct __attribute__((packed)) {
+    char vehicle_id[12];
+    float lat;
+    float lon;
+    uint8_t is_parking;
+    uint64_t time;
+} parking_message_t;
+#pragma pack()  // Restore default packing
+
+#endif // PARK_MESSAGE_H
